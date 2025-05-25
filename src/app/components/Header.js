3664070c3e180225e24fba5a5bdec2b25df0f96c
kevvj <kevinjvillalba774+1@gmail.com>
2025-05-24 19:35:15 -0500

    Update Header.js

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -3,25 +3,34 @@ import { useRouter } from 'next/navigation'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFaceAngry } from '@fortawesome/free-regular-svg-icons'
 import { faGraduationCap } from '@fortawesome/free-solid-svg-icons'
+import { useEffect, useState } from 'react'
 
-const Header = ()=>{
+const Header = () => {
+    const [isLoad, setIsLoad] = useState(false)
     const router = useRouter()
 
+    useEffect(() => {
+        setIsLoad(true)
+    }, [])
+
     //aqui la mayoria de cosas faltan
-    return(
+    return (
         <header className='header'>
-        <span className='icon' onClick={()=> router.push('/')}><FontAwesomeIcon icon={faGraduationCap} />UnicorHub</span>
+            <span className='icon' onClick={() => router.push('/')}>
 
-        <ul className='header-nav'>
-            <li>Material</li>
-            <li>Recursos</li>
-            <li>Documentos</li>
-            <li>Biblioteca</li>
-            <li>Foro</li>
-        </ul>
+                {isLoad && <FontAwesomeIcon icon={faGraduationCap} />}
+                UnicorHub</span>
 
-        <input type="text" className='header-search'></input>
-        <button onClick={()=>router.push('/auth/sign-in')} className='header-login'>LogIn</button>
+            <ul className='header-nav'>
+                <li>Material</li>
+                <li>Recursos</li>
+                <li>Documentos</li>
+                <li>Biblioteca</li>
+                <li>Foro</li>
+            </ul>
+
+            <input type="text" className='header-search'></input>
+            <button onClick={() => router.push('/auth/sign-in')} className='header-login'>LogIn</button>
         </header>
     )
 }
