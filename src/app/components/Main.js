f794700909aa27aef37c03b6733f5d264b0d053a
kevvj <kevinjvillalba774+1@gmail.com>
2025-05-24 19:29:21 -0500

    Update Main.js

--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,38 +1,23 @@
 
 import { useRouter } from 'next/navigation'
 import { CareerSP } from '../career/[id]/hooks/useContent'
+import { useEffect, useState } from 'react'
 const Main = () => {
 
-    const prueba = CareerSP()
+    const [careers, setCareers] = useState([])
+
+    useEffect(() =>{
+        const getCareers = async () =>{
+          const result = await CareerSP()
+          setCareers(result)
+        }
+
+        getCareers()
+    },[])
 
     //esperen lo meto a la base de datos
 
-    const careers = [
-        {
-            id: "Engineering", name: "Ingeniería", description: "Ingeniería es la aplicación práctica de conocimientos científicos y matemáticos para diseñar, construir y optimizar estructuras, máquinas, sistemas o procesos que solucionan problemas y mejoran la vida humana."
-        },
-        {
-            id: "Medicine", name: "Medicina", description: "Medicina es la ciencia y práctica dedicada al diagnóstico, tratamiento y prevención de enfermedades para mejorar la salud y el bienestar de las personas."
-        },
-        {
-            id: "Arquitecture", name: "Arquitectura", description: "Arquitectura es el arte y técnica de diseñar y construir edificios y espacios que combinan funcionalidad, estética y seguridad."
-        },
-        {
-            id: "Law", name: "Derecho", description: "Derecho es el estudio y aplicación de normas y leyes que regulan la convivencia social y la justicia."
-        },
-        {
-            id: "Psychology", name: "Psicología", description: "Psicología es la ciencia que estudia el comportamiento y los procesos mentales humanos para mejorar la salud mental y emocional."
-        },
-        {
-            id: "Economy", name: "Economía", description: "Economía es el análisis de la producción, distribución y consumo de bienes y servicios para entender y mejorar el bienestar social."
-        },
-        {
-            id: "Computer Engineering", name: "Ingeniería Informática", description: "Ingeniería Informática se enfoca en el diseño, desarrollo y mantenimiento de sistemas y software para resolver problemas tecnológicos."
-        },
-        {
-            id: "Nursing", name: "Enfermería", description: "Enfermería es la disciplina dedicada al cuidado integral de las personas para promover la salud y asistir en la recuperación de enfermedades."
-        }
-    ]
+    
 
     const router = useRouter()
 
