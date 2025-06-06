cef5a2c6589b172dedb398fff765b1331a141a08
kevvj <kevinjvillalba774+1@gmail.com>
2025-06-06 15:50:08 -0500

    a

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -4,12 +4,23 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFaceAngry } from '@fortawesome/free-regular-svg-icons'
 import { faGraduationCap } from '@fortawesome/free-solid-svg-icons'
 import { useEffect, useState } from 'react'
+import supabase from '../hooks/supabase'
+import { faCircleUser } from '@fortawesome/free-regular-svg-icons'
 
 const Header = () => {
     const [isLoad, setIsLoad] = useState(false)
+    const [user, setUser] = useState(false)
+
     const router = useRouter()
 
+
+
     useEffect(() => {
+        const isLoggedIn = async () => {
+            const { data: { user } } = await supabase.auth.getUser()
+            setUser(user)
+        }
+        isLoggedIn()
         setIsLoad(true)
     }, [])
 
@@ -24,13 +35,17 @@ const Header = () => {
             <ul className='header-nav'>
                 <li>Material</li>
                 <li>Recursos</li>
-                <li onClick = {() => router.push('/upload')}>Documentos</li>
+                <li onClick={() => router.push('/upload')}>Documentos</li>
                 <li>Biblioteca</li>
                 <li>Foro</li>
             </ul>
 
             <input type="text" className='header-search'></input>
-            <button onClick={() => router.push('/auth/sign-in')} className='header-login'>LogIn</button>
+            {!user ? <button onClick={() => router.push('/auth/sign-in')} className='header-login'>LogIn</button> : 
+            <div onClick={() => router.push('/auth/sign-in')} className='header-icon'>
+                <FontAwesomeIcon icon={faCircleUser} size='2x'></FontAwesomeIcon >
+            </div>
+            }
         </header>
     )
 }
